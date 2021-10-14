8e99517d7148432fa5e3a8786d1ce63ac36cc544
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-10-14 16:38:47 +0200

    v1.3
    Add complete route
    Add complete icon changing on hover
    Edit todo display in home page to only display not completed tasks

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import datetime
+
 from flask import Flask, render_template, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 from werkzeug.utils import redirect
-from wtforms import StringField, validators, SubmitField
+from wtforms import StringField, validators
 
 app = Flask(__name__)
 
@@ -18,6 +20,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(120), unique=True, nullable=False)
+    date_completed = db.Column(db.String(120), nullable=True)
 
 
 # render_kw={'autofocus': True} to set the cursor focused in the field
@@ -28,8 +31,8 @@ class AddTodoForm(Form):
 class EditTodoForm(Form):
     task = StringField(validators=[validators.input_required()], render_kw={'autofocus': True})
 
-db.create_all()
 
+db.create_all()
 
 
 @app.route("/", methods=['POST', 'GET'])
@@ -60,15 +63,25 @@ def home():
     return render_template('index.html', form=form, todos=todos)
 
 
-
-
-
 @app.route("/delete/task/<int:task_id>")
 def delete(task_id):
     Todo.query.filter_by(id=task_id).delete()
     db.session.commit()
 
     flash('Task successfully deleted')
+
+    return redirect(url_for('home'))
+
+
+@app.route("/complete/task/<int:task_id>")
+def complete(task_id):
+    todo_to_edit = Todo.query.get(task_id)
+
+    todo_to_edit.date_completed = datetime.datetime.today().strftime("%m/%d/%Y")
+
+    db.session.commit()
+
+    flash('Task successfully completed')
 
     return redirect(url_for('home'))
 
@@ -80,10 +93,8 @@ def edit(task_id):
     todo_to_edit = Todo.query.get(task_id)
     task_to_edit = todo_to_edit.task
 
-
     if form.validate_on_submit():
         new_task = form.task.data
-
 
         todo_to_edit.task = new_task
         db.session.commit()
@@ -92,11 +103,7 @@ def edit(task_id):
 
         return redirect(url_for('home'))
 
-
     return render_template('edit.html', form=form, task=task_to_edit)
-
-
-# T0DO recherche button, flash on every single action, checkbox for task done and move it to finished task part?
 
 
 if __name__ == '__main__':
